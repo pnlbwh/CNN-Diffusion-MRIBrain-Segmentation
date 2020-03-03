4e8fa2b199acfecaa8491019e80bfc6afa40a249
senthilcaesar <senthilcaesar@yahoo.co.in>
2020-03-03 10:52:21 -0500

    code added to make training easy for user

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from compnet import train_model
+from settings import Settings
+
+
+if __name__ == '__main__':
+    
+    settings = Settings()
+
+    data_params = settings['DATA']
+    train_params = settings['TRAINING']
+    common_params = settings['COMMON']
+
+    train_model(data_params, train_params, common_params)
